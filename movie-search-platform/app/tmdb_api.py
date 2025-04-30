c07111b5399b61b83a5a9ba1b7498f59306316c4
Anesu Rirwa <anerirwa@gmail.com>
2025-04-30 22:02:46 +0200

    Added move fetch capability using the TMDB API in the tmdb_api.py file

---
+++ movie-search-platform/app/tmdb_api.py
@@ -0,0 +1,28 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+API_KEY = os.getenv("TMDB_API_KEY")
+# READ_ACCESS_TOKEN = os.getenv("TMDB_READ_ACCESS_TOKEN")
+BASE_URL = "https://api.themoviedb.org/3/search/authentication"
+
+import requests
+
+url = "https://api.themoviedb.org/3/search/movie"
+params = {
+    "query": "Inception",           # ← This is the movie title you're searching for
+    "include_adult": False,
+    "language": "en-US",
+    "page": 1
+}
+headers = {
+    "Authorization": f"Bearer {API_KEY}",  # Use your TMDb v4 access token here
+    "accept": "application/json"
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print(response.text)
